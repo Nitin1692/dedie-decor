0158e243b8a83304304a2c5c57c70c7ddc93b4de
nitin <nitinjain7201@gmail.com>
2022-06-03 00:01:16 +0530

    changes made in button

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const buttons = document.querySelector("[data-carousel-button]");
+const buttons = document.querySelectorAll("[data-carousel-button]");
 
 buttons.forEach(button => {
     button.addEventLister("click", ()=>{
         const offset = button.dataset.carsouelButton === "next" ? 1 : -1
-        const slides = button.closest("[data-carousel").querySelector('[data-slides]')
-        const activeSlides = document.querySelector("[data-active]")
+        const slides = button.closest("[data-carousel]").querySelector("[data-slides]")
+        const activeSlides = slides.querySelector("[data-active");
         let newIndex = [...slides.children].indexOf(activeSlides) + offset;
         if (newIndex < 0) {
             newIndex = slides.children.length - 1
