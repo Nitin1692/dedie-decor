39e3d9cf422cd27f22ba207cbf6c3ca7df6b405a
nitin <nitinjain7201@gmail.com>
2022-07-22 18:15:51 +0530

    Added cursor

--- productjs.js
+++ productjs.js
@@ -8,8 +8,3 @@ $(document).ready(function() {
     });  
   });
 
-var cursor = document.getElementById('cursor');
-var cursor2 = document.getElementById('cursor2');
-document.addEventListener('mousemove', function(e){
-   cursor.style.cssText = cursor2.style.cssText = "left: " + e.clientX + "px; top: " + e.clientY + "px;"
-});
