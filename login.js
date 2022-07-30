d6aecf81b92d7323fa875fe7b893af0f5079c5e5
nitin <nitinjain7201@gmail.com>
2022-07-30 16:50:00 +0530

    Added login page

--- login.js
+++ login.js
@@ -1,3 +1,14 @@
 $('.message a').click(function(){
     $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
  });
+
+
+ var login = document.getElementById('Login');
+
+
+ login.addEventListener('click', (e)=>{
+    e.preventDefault();
+    setTimeout(() => {
+        window.location.href='index.html';
+    }, 1000);
+ })
