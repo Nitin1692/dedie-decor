c1b4d05e62684a35ac3a9b60ce9a8f0c370c5d20
nitin <nitinjain7201@gmail.com>
2022-07-31 14:26:41 +0530

    Added cursor in login page

--- login.js
+++ login.js
@@ -12,3 +12,9 @@ $('.message a').click(function(){
         window.location.href='index.html';
     }, 1000);
  })
+
+ var cursor = document.getElementById('cursor');
+var cursor2 = document.getElementById('cursor2');
+document.addEventListener('mousemove', function(e){
+   cursor.style.cssText = cursor2.style.cssText = "left: " + e.clientX + "px; top: " + e.clientY + "px;"
+});
